85dbb71facf1f67b842ddb794676b12724ab919d
yavuzk28 <yavuzk28@proton.me>
2026-07-09 16:56:15 +0200

    Donwload Repos Script Added

---
+++ Scripts/download_repos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE="$SCRIPT_DIR/../Datasets/Source"
+
+rm -rf "$BASE"
+mkdir -p "$BASE"
+cd "$BASE"
+
+
+REPOS=(
+    "https://github.com/rxi/fe.git"
+    "https://github.com/brenns10/lsh.git"
+    "https://github.com/mevdschee/2048.c.git"
+    "https://github.com/antirez/kilo.git"
+    "https://github.com/Zunawe/md5-c.git"
+)
+
+
+for repo in "${REPOS[@]}"; do
+    git clone "$repo"
+done
+
